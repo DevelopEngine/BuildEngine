6b018c86461ff5d6013b6b633b0553a5a94e4106
Alistair Chapman <alistair@agchapman.com>
2021-01-16 19:20:54 +1000

    Fix broken UA logic

--- src/BuildEngine/Scripts/ScriptDownloadService.cs
+++ src/BuildEngine/Scripts/ScriptDownloadService.cs
@@ -11,65 +11,59 @@ namespace BuildEngine.Scripts {
         public Task<string> GetScriptPath();
         public Task<BuildScript> GetScriptContext(string targetPath);
     }
-    public abstract class ScriptDownloadBase {
+    public abstract class ScriptDownloadBase
+    {
         public string UserAgent { get; protected set; }
-        protected virtual string ScriptFilePath {get;}
+        protected virtual string ScriptFilePath { get; }
         private readonly ILogger<IScriptService> _logger;
         private (string Name, string Version) _appName;
-        private IAppInfoProvider _infoProvider;
+        private IAppInfoProvider _infoProvider { get; set; } = new EntryAppInfoProvider();
 
-        protected virtual string ScriptFileName {get;}
+        protected virtual string ScriptFileName { get; }
 
-        protected ScriptDownloadBase(string fileName, ILogger<IScriptService> logger) {
+        protected ScriptDownloadBase(string fileName, ILogger<IScriptService> logger)
+        {
             ScriptFileName = fileName;
             ScriptFilePath = Path.Combine(GetTempPath(), ScriptFileName);
             _logger = logger;
         }
 
-        protected ScriptDownloadBase(string fileName, ILogger<IScriptService> logger, IAppInfoProvider infoProvider) : this(fileName, logger) {
+        protected ScriptDownloadBase(string fileName, ILogger<IScriptService> logger, IAppInfoProvider infoProvider) : this(fileName, logger)
+        {
             _infoProvider = infoProvider;
-            if (_infoProvider != null && _infoProvider.GetAppInfo() is var appInfo && appInfo.Name != null) {
-                UserAgent = $"{appInfo.Name}/{appInfo.Version}";
-            }
         }
 
-        [Obsolete("This has been replaced by an IAppInfoProvider")]
-        private void ParseAppName(string appName) {
-            AssemblyName GetEntryAssembly() {
-                var assemblyName = Assembly.GetEntryAssembly().GetName();
-                return assemblyName;
-            }
-            if (string.IsNullOrWhiteSpace(appName)) {
-                var entry = GetEntryAssembly();
-                _appName = (entry.Name, entry.Version.ToString(3));
-            } else if (!appName.Contains("/")) {
-                var entry = GetEntryAssembly();
-                _appName = (appName, entry.Version.ToString(3));
-            } else {
-                var parts = appName.Split('/');
-                _appName = (parts.First(), parts.Last());
-            }
-        }
-
-        private string GetTempPath() {
+        private string GetTempPath()
+        {
             var tempDir = Path.Combine(Path.GetTempPath(), _appName.Name ?? "build");
             var di = Directory.CreateDirectory(tempDir);
             return di.FullName;
         }
 
-        protected async Task DownloadScript(string url) {
+        private string GetUserAgent() {
+            if (_infoProvider != null && _infoProvider.GetAppInfo() is var appInfo && appInfo.Name != null)
+            {
+                return $"{appInfo.Name}/{appInfo.Version}";
+            }
+            throw new Exception("User agent could not be determined!");
+        }
+
+        protected async Task DownloadScript(string url)
+        {
             _logger.LogDebug($"Downloading new build file: '{ScriptFileName}'");
             using var client = GetClient();
             // var file = await client.GetStringAsync(url);
             // await File.WriteAllTextAsync(_scriptFilePath, file);
             var response = await client.GetAsync(url);
             using var fs = new FileStream(
-                ScriptFilePath, 
+                ScriptFilePath,
                 FileMode.Create);
             await response.Content.CopyToAsync(fs);
         }
 
-        private HttpClient GetClient() {
+        private HttpClient GetClient()
+        {
+            UserAgent ??= GetUserAgent();
             var client = new HttpClient();
             client.DefaultRequestHeaders.Add("User-Agent", UserAgent);
             return client;
